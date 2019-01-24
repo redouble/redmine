c43c8203063dd0b4760f84bb3637f8809d7b55d9
Dong Zhen <redouble@outlook.com>
2019-01-24 16:46:47 +0800

    appname & url

--- lib/redmine/info.rb
+++ lib/redmine/info.rb
@@ -1,8 +1,8 @@
 module Redmine
   module Info
     class << self
-      def app_name; 'Redmine' end
-      def url; 'https://www.redmine.org/' end
+      def app_name; '石刻项目' end
+      def url; 'http://shike.in/' end
       def help_url; 'https://www.redmine.org/guide' end
       def versioned_name; "#{app_name} #{Redmine::VERSION}" end
 
